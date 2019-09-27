7627955b3ad938056efa70e1818e1237f6ae9601
Roman Melenchenko <melenchenko@yandex.ru>
2019-09-27 22:55:25 +0300

    Updates

---
+++ precache-manifest.0dcb0174715229aa34b6a35b21a22d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "765e71bbcfa85cbee54fbe1fd56628e9",
+    "url": "/hk2vk/index.html"
+  },
+  {
+    "revision": "cd84aa1d73dbc52e3920",
+    "url": "/hk2vk/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "300e89f7666f1f6b4441",
+    "url": "/hk2vk/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "cd84aa1d73dbc52e3920",
+    "url": "/hk2vk/static/js/2.3598faee.chunk.js"
+  },
+  {
+    "revision": "300e89f7666f1f6b4441",
+    "url": "/hk2vk/static/js/main.9ff26b98.chunk.js"
+  },
+  {
+    "revision": "a389d6e8267a9ac3ecb0",
+    "url": "/hk2vk/static/js/runtime-main.16254e04.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/hk2vk/static/media/persik.4e1ec840.png"
+  }
+]);
