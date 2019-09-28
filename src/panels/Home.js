ac47ac3a98d37f719e3d3eea27fd01c1e4d19136
Roman Melenchenko <melenchenko@yandex.ru>
2019-09-28 22:26:50 +0300

    fetchedUser log

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -76,6 +76,7 @@ class Home extends React.Component {
 		quests.forEach((item) => {
 			quest_list.push(<Div key={item.id} data-quest={item.id} onClick={goQuest}>{item.title}</Div>);
 		});
+		console.log(fetchedUser);
 		let result = <Panel id={id}>
 			<PanelHeader 
 				left={<HeaderButton key="addquest"><Icon24Add onClick={go} data-to="addquest"/></HeaderButton>}>
